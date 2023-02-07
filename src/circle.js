f0d6539d5e9865945fd98fe3b883c2dea177ade4
TEKFA Fouad <fouad.tekfa@etu.univ-lehavre.fr>
2023-02-07 20:57:27 +0100

    le rayon doit etre positive

--- src/circle.js
+++ src/circle.js
@@ -8,17 +8,24 @@ export function Circle(x,y,radius) {
     let RadiusGlobal=radius;
     let tab=[XGlobal,YGlobal];
 
-    let creatProperty=(prop,variable,$this)=>{
+    let creatProperty=(prop,$this,getFunction,setFunction)=>{
 
         Object.defineProperty($this, prop, {
-            get: ()=>  variable,
+            get: getFunction,
+            set : setFunction,
             enumerable : false,
         });
 
     }
-    creatProperty('x',XGlobal,this);
-    creatProperty('y',YGlobal,this);
-    creatProperty('radius',RadiusGlobal,this);
+    creatProperty('x',this,() => XGlobal);
+    creatProperty('y',this,() => YGlobal);
+    creatProperty('radius',this,() => RadiusGlobal,
+    (val) => {
+        if(val >= 0) RadiusGlobal = val;
+         else throw new Error("le rayon doit etre positive");
+
+    });
+
    //creatProperty('coords',tab,this);
     Object.defineProperty(this, 'coords', {
         get: ()=>  tab,
