0ea84389f17423d0bbd1a4412b831075bbda0bc1
TEKFA Fouad <fouad.tekfa@etu.univ-lehavre.fr>
2023-02-07 20:41:35 +0100

    moveTo passed

--- src/circle.js
+++ src/circle.js
@@ -7,19 +7,33 @@ export function Circle(x,y,radius) {
     let YGlobal=y;
     let RadiusGlobal=radius;
     let tab=[XGlobal,YGlobal];
+
     let creatProperty=(prop,variable,$this)=>{
 
         Object.defineProperty($this, prop, {
             get: ()=>  variable,
-            enumerable : false
+            enumerable : false,
         });
 
     }
     creatProperty('x',XGlobal,this);
     creatProperty('y',YGlobal,this);
     creatProperty('radius',RadiusGlobal,this);
-    creatProperty('coords',tab,this);
+   //creatProperty('coords',tab,this);
+    Object.defineProperty(this, 'coords', {
+        get: ()=>  tab,
+        enumerable : false,
+    });
 
+
+    Object.defineProperty(this, 'moveTo', {
+        value: (x ,y)=>{
+            XGlobal=x;
+            YGlobal=y;
+            tab=[XGlobal,YGlobal];
+        },
+        enumerable : false,
+    });
 
 
 
